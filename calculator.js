b75fccd43b1d4b327e7761af0f03632708285c76
Duy-N-Nguyen <aznduynguyen@gmail.com>
2023-11-06 18:47:53 -0500

    fff

--- calculator.js
+++ calculator.js
@@ -1,49 +1,8 @@
-let currentInput = '';
-let currentOperator = '';
-let previousInput = '';
-
-function appendValue(value) {
-    currentInput += value;
-    updateDisplay();
-}
-
-function clearDisplay() {
-    currentInput = '';
-    currentOperator = '';
-    previousInput = '';
-    updateDisplay();
-}
-
-function calculate() {
-    if (currentInput && currentOperator && previousInput) {
-        currentInput = operate(previousInput, currentInput, currentOperator);
-        currentOperator = '';
-        previousInput = '';
-        updateDisplay();
-    }
-}
-
-function operate(a, b, operator) {
-    a = parseFloat(a);
-    b = parseFloat(b);
-
-    switch (operator) {
-        case '+':
-            return a + b;
-        case '-':
-            return a - b;
-        case '*':
-            return a * b;
-        case '/':
-            if (b === 0) return 'Error';
-            return a / b;
-        default:
-            return 'Error';
-    }
-}
-
-function updateDisplay() {
-    document.getElementById('display').value = currentInput;
-}
-
-updateDisplay();
+// JavaScript "Hello, World!" example
+function sayHello() {
+    alert("Hello, World!");
+  }
+  
+  // Call the function to display the alert
+  sayHello();
+  

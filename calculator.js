c03261e0d8d2e64553c7460f60bb9c29c52ba370
Duy-N-Nguyen <aznduynguyen@gmail.com>
2023-11-07 16:28:04 -0500

    almost done coloring

--- calculator.js
+++ calculator.js
@@ -1,8 +1,30 @@
-// JavaScript "Hello, World!" example
-function sayHello() {
-    alert("Hello, World!");
-  }
-  
-  // Call the function to display the alert
-  sayHello();
-  
+// JavaScript functions for your calculator
+
+let isOpenParenthesis = false;
+
+function toggleParenthesis() {
+    const button = document.getElementById('parenthesisButton');
+    if (isOpenParenthesis) {
+        document.getElementById('display').value += ')';
+    } else {
+        document.getElementById('display').value += '(';
+    }
+    isOpenParenthesis = !isOpenParenthesis;
+}
+
+function calculateResult() {
+    try {
+        const result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+
+function calculatePercentage() {
+    const currentValue = parseFloat(document.getElementById('display').value);
+    if (!isNaN(currentValue)) {
+        const percentageValue = currentValue / 100;
+        document.getElementById('display').value = percentageValue;
+    }
+}

ad5a99d6a4af9fdfbaf6b0105a3920583794ae5e
Duy-N-Nguyen <aznduynguyen@gmail.com>
2023-11-08 11:54:38 -0500

    update calc code

--- calculator.js
+++ calculator.js
@@ -1,16 +1,37 @@
 // JavaScript functions for your calculator
 
-let isOpenParenthesis = false;
-
+var left_parentheses_counter = 0;
 function toggleParenthesis() {
-    const button = document.getElementById('parenthesisButton');
-    if (isOpenParenthesis) {
-        document.getElementById('display').value += ')';
-    } else {
-        document.getElementById('display').value += '(';
+    const display = document.getElementById('display');
+    const displayValue = display.value;
+    if (displayValue.length === 0 || displayValue.charAt(displayValue.length - 1) === '(') {
+        display.value += '(';
+        left_parentheses_counter++;
     }
-    isOpenParenthesis = !isOpenParenthesis;
+    else if (/^[0-9]$/.test(displayValue.charAt(displayValue.length - 1)) && left_parentheses_counter > 0) {
+        display.value += ')';
+        left_parentheses_counter--;
+    }
+    else if (left_parentheses_counter == 0) {
+        display.value += '(';
+        left_parentheses_counter++;
+    }
+    else {
+        display.value += ')';
+        left_parentheses_counter--;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
 
 function calculateResult() {
     try {
@@ -21,6 +42,15 @@ function calculateResult() {
     }
 }
 
+
+
+
+
+
+
+
+
+
 function calculatePercentage() {
     const currentValue = parseFloat(document.getElementById('display').value);
     if (!isNaN(currentValue)) {
@@ -28,3 +58,5 @@ function calculatePercentage() {
         document.getElementById('display').value = percentageValue;
     }
 }
+
+

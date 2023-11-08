0727b2c12e8e060519e16cc3f0da637172b7afed
Duy-N-Nguyen <aznduynguyen@gmail.com>
2023-11-08 14:58:38 -0500

    f

--- calculator.js
+++ calculator.js
@@ -1,5 +1,3 @@
-// JavaScript functions for your calculator
-
 var left_parentheses_counter = 0;
 function toggleParenthesis() {
     const display = document.getElementById('display');
@@ -23,20 +21,43 @@ function toggleParenthesis() {
 }
 
 function calculateResult() {
+    const display = document.getElementById('display');
+    const expression = display.value;
+
     try {
-        const result = eval(document.getElementById('display').value);
-        document.getElementById('display').value = result;
+        const result = evaluateExpression(expression);
+        display.value = result;
     } catch (error) {
-        document.getElementById('display').value = 'Error';
+        display.value = 'Error';
     }
 }
 
-function calculatePercentage() {
-    const currentValue = parseFloat(document.getElementById('display').value);
-    if (!isNaN(currentValue)) {
-        const percentageValue = currentValue / 100;
-        document.getElementById('display').value = percentageValue;
+function evaluateExpression(expression) {
+    // Use regular expressions to replace invalid characters for security
+    const sanitizedExpression = expression.replace(/[^0-9+\-*/().]/g, '');
+
+    // Check for balanced parentheses
+    if (!areParenthesesBalanced(sanitizedExpression)) {
+        throw new Error('Unbalanced parentheses');
     }
+
+    // Use the JavaScript `eval` function to evaluate the expression
+    return eval(sanitizedExpression);
 }
 
+function areParenthesesBalanced(expression) {
+    const stack = [];
 
+    for (let i = 0; i < expression.length; i++) {
+        if (expression[i] === '(') {
+            stack.push('(');
+        } else if (expression[i] === ')') {
+            if (stack.length === 0) {
+                return false; // Unbalanced closing parenthesis
+            }
+            stack.pop();
+        }
+    }
+
+    return stack.length === 0; // Stack should be empty for balanced parentheses
+}
